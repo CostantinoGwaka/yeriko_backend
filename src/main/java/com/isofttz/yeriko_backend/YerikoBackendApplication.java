331526a77a00d18be016a73517ff7c930c277dfe
Costantino Dionis Gwaka <gwaka94@gmail.com>
2025-06-25 15:17:53 +0300

    fix init app

---
+++ src/main/java/com/isofttz/yeriko_backend/YerikoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.isofttz.yeriko_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YerikoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YerikoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isofttz/yeriko_backend/YerikoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isofttz.yeriko_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YerikoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
